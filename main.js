6d629e9cfb4d216b3efac0439450ef1597ee8d5e
tabspear <tabspear@github.com>
2022-08-24 14:24:16 -0700

    Feat(client) add citation and make the dev and rel tabs run

--- main.js
+++ main.js
@@ -3,7 +3,9 @@
 //Create tge fetch statement
 // test and modify
 let btn = document.getElementById("but_n");
-btn.addEventListener("click", () => {
+const rand_quotes = document.getElementById("rand_q");
+rand_quotes.addEventListener("click", () => {
+  btn.addEventListener("click", () => {
   fetch("random.json")
     .then((res) => {
       return res.json();
@@ -11,14 +13,20 @@ btn.addEventListener("click", () => {
     .then((data) => {
       let ArabicText = document.getElementById("Arabic_quote");
       let EnglishText = document.getElementById("English_quote");
+      let citation = document.getElementById("cit_ion")
       let randomNum = Math.floor(Math.random() * data.length);
       let item = data[randomNum];
-      ArabicText.innerHTML = `${item.Arabic}`;
-      EnglishText.innerHTML = `${item.English}`
-  
+        ArabicText.innerHTML = `${item.Arabic}`;
+        EnglishText.innerHTML = `${item.English}`
+        citation.innerHTML = `${item.citation}, ${item.year}`
+      })
+
+
       runAnimate();
     });
-});
+})
+
+
 
 function runAnimate() {
   let ArabicText = document.getElementById("Arabic_quote");
@@ -27,3 +35,50 @@ function runAnimate() {
   EnglishText.classList.add("tracking-in-expand-fwd");
   ArabicText.classList.add("tracking-in-expand-fwd");
 }
+
+// Dev/ rel quotes, click on the dev button, fetch dev quotes, randomly generate them, test
+
+const relQuotes = document.getElementById("rel_q")
+relQuotes.addEventListener('click', () => {
+  
+  btn.addEventListener("click", () => {
+  fetch("rel.json")
+    .then((res) => {
+      return res.json()
+    }).then((data) => {
+      let ArabicText = document.getElementById("Arabic_quote");
+      let EnglishText = document.getElementById("English_quote");
+      let citation = document.getElementById("cit_ion")
+      let randomNum = Math.floor(Math.random() * data.length);
+      let item = data[randomNum];
+        ArabicText.innerHTML = `${item.Arabic}`;
+        EnglishText.innerHTML = `${item.English}`
+        citation.innerHTML = `${item.citation}, ${item.year}`
+
+        runAnimate();
+      })
+
+    })
+})
+
+const devQuotes = document.getElementById("dev_q")
+devQuotes.addEventListener('click', () => {
+  btn.addEventListener("click", () => {
+  fetch("dev.json")
+    .then((res) => {
+      return res.json()
+    }).then((data) => {
+      console.log(data)
+      let ArabicText = document.getElementById("Arabic_quote");
+      let EnglishText = document.getElementById("English_quote");
+      let citation = document.getElementById("cit_ion")
+      let randomNum = Math.floor(Math.random() * data.length);
+      let item = data[randomNum];
+        ArabicText.innerHTML = "",
+        EnglishText.innerHTML = `${item.English}`
+        citation.innerHTML = `${item.citation}, ${item.year}`
+        runAnimate();
+      })
+
+    })
+})
